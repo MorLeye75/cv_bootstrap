fe2d2dcbd59e7d4c29ee599af1cdbadd8dfc832b
Morleye Max <morleye360@gmail.com>
2025-12-31 22:57:58 +0000

    Partie 4.II: Intégration HTML et ressources avec SCSS

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule], // Ajoute CommonModule
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class AppComponent {
   protected readonly title = signal('cv_bootstrap');
 }
