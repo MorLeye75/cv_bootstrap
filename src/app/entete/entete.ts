582ccfe5a6510e0b13cdab4409623d094d85b8d5
Morleye Max <morleye360@gmail.com>
2025-12-31 23:17:05 +0000

    Partie 4.IV: Création du composant entete-cv

---
+++ src/app/entete/entete.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-entete',
+  imports: [],
+  templateUrl: './entete.html',
+  styleUrl: './entete.scss',
+})
+export class Entete {
+
+}
